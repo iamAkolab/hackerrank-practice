c8283d88ba6c226995c85ea56f6d0170808bc3b3
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-11-02 00:02:15 -0500

    Create designer_door_Mat.py

---
+++ designer_door_Mat.py
@@ -0,0 +1,68 @@
+"""
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+Mat size must be N X M ( N is an odd natural number, and M is 3 times N.)
+The design should have 'WELCOME' written in the center.
+The design pattern should only use |, . and - characters.
+
+Sample Designs
+
+    Size: 7 x 21 
+    ---------.|.---------
+    ------.|..|..|.------
+    ---.|..|..|..|..|.---
+    -------WELCOME-------
+    ---.|..|..|..|..|.---
+    ------.|..|..|.------
+    ---------.|.---------
+    
+    Size: 11 x 33
+    ---------------.|.---------------
+    ------------.|..|..|.------------
+    ---------.|..|..|..|..|.---------
+    ------.|..|..|..|..|..|..|.------
+    ---.|..|..|..|..|..|..|..|..|.---
+    -------------WELCOME-------------
+    ---.|..|..|..|..|..|..|..|..|.---
+    ------.|..|..|..|..|..|..|.------
+    ---------.|..|..|..|..|.---------
+    ------------.|..|..|.------------
+    ---------------.|.---------------
+    
+Input Format
+
+A single line containing the space separated values of  and .
+
+Constraints
+
+
+Output Format
+
+Output the design pattern.
+
+Sample Input
+Sample Input
+
+9 27
+Sample Output
+
+------------.|.------------
+---------.|..|..|.---------
+------.|..|..|..|..|.------
+---.|..|..|..|..|..|..|.---
+----------WELCOME----------
+---.|..|..|..|..|..|..|.---
+------.|..|..|..|..|.------
+---------.|..|..|.---------
+------------.|.------------
+"""
+
+
+# ========================
+#         Solution
+# ========================
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n, m = map(int,input().split())
+pattern = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
+print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
