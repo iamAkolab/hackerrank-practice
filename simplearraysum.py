542b006b7267a4ff5fdac32de0e424ba7080de40
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-12-01 00:04:12 -0600

    Update simplearraysum.py

--- simplearraysum.py
+++ simplearraysum.py
@@ -19,14 +19,10 @@ def simpleArraySum(ar):
         x = x + ar[i]
     return x
         
-
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     ar_count = int(input().strip())
-
     ar = list(map(int, input().rstrip().split()))
-
     result = simpleArraySum(ar)
 
     fptr.write(str(result) + '\n')
