f34ae3135946cf9425aa07ebb6df854b47169506
Abdullah Akinde <77028341+iamAkolab@users.noreply.github.com>
2021-12-06 23:35:11 -0600

    Update countingValleys.py

--- countingValleys.py
+++ countingValleys.py
@@ -1,3 +1,42 @@
+"""
+Gary is an avid hiker. He tracks his hikes meticulously, paying close attention to small details like topography. 
+During his last hike he took exactly n steps. For every step he took, he noted if it was an uphill, U , or a downhill, D step.
+
+Gary's hikes start and end at sea level and each step up or down represents a 1 unit change in altitude. We define the following terms:
+
+A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
+A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.
+
+Given Gary's sequence of up and down steps during his last hike, find and print the number of valleys he walked through.
+
+For example, 
+
+steps = 8 path = [DDUUUUDD]
+
+The hiker first enters a valley 2 units deep. Then they climb out and up onto a mountain 2 units high. Finally, the hiker returns to sea level and ends the hike.
+
+
+Function Description
+
+Complete the countingValleys function in the editor below. It must return an integer that denotes the number of valleys Gary traversed.
+countingValleys has the following parameter(s):
+
+countingValleys has the following parameter(s):
+
+int steps: the number of steps on the hike
+string path: a string describing the path
+
+
+Returns
+
+int: the number of valleys traversed
+
+Input Format
+The first line contains an integer , the number of steps in Gary's hike. 
+The second line contains a single string , of  characters that describe his path.
+Constraints
+
+"""
 
 #!/bin/python3
 
